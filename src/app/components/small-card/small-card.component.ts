538e588667dac7974ef0519000fb9769adc115cc
kelvenwyllames <kelvenwyllames@gmail.com>
2023-10-16 11:08:48 -0300

    components dinamycs propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent implements OnInit {
+  @Input()
+  smallPhotoCover: string =
+    'https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg';
 
-  constructor() { }
+  @Input()
+  smallDateCard: string = '15 de Outubro, 2023';
 
-  ngOnInit(): void {
-  }
+  @Input()
+  smallCardTitle: string =
+    'Beberapa cara menggunakan bold typography pada design website';
+  constructor() {}
 
+  ngOnInit(): void {}
 }
